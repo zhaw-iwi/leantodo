e98e1ded93b7e9107c262c9797b35d824fadbcc8
peterheinrich <peter.heinrich@its-heinrich.ch>
2024-02-05 15:23:20 +0100

    Updating docker config

--- backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/boundary/ProjectEndpoint.java
+++ backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/boundary/ProjectEndpoint.java
@@ -3,14 +3,13 @@ package ch.zhaw.sml.iwi.meng.leantodo.boundary;
 import java.security.Principal;
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ch.zhaw.sml.iwi.meng.leantodo.controller.ProjectController;
@@ -32,7 +31,7 @@ public class ProjectEndpoint {
     
     @RequestMapping(path = "/api/project/{id}", method = RequestMethod.POST)
     @PreAuthorize("hasRole('USER')")
-    public void addToDo(@RequestParam(name="id") Long projectId, @RequestBody ToDo toDo,  Principal principal) {
+    public void addToDo(@PathVariable(name="id") Long projectId, @RequestBody ToDo toDo,  Principal principal) {
         projectController.addToDo(projectId, toDo, principal.getName());
     }
 }
--- backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/boundary/ToDoEndpoint.java
+++ backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/boundary/ToDoEndpoint.java
@@ -2,10 +2,15 @@ package ch.zhaw.sml.iwi.meng.leantodo.boundary;
 
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import ch.zhaw.sml.iwi.meng.leantodo.controller.ToDoController;
 import ch.zhaw.sml.iwi.meng.leantodo.entity.ToDo;
+import ch.zhaw.sml.iwi.meng.leantodo.entity.ToDoRepository;
 
 @RestController
 @CrossOrigin
@@ -20,6 +26,23 @@ public class ToDoEndpoint {
 
     @Autowired
     private ToDoController toDoController;
+
+    @Autowired
+    private ToDoRepository toDoRepository;
+
+
+    @RequestMapping(path = "/api/todo/{id}", method = RequestMethod.GET)
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<ToDo> getToDo(@PathVariable(name = "id") Long toDoId, Principal principal) {
+        Optional<ToDo> resOpt = toDoRepository.findById(toDoId);
+
+        if(resOpt.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        else {        
+            return  new ResponseEntity<>(resOpt.get(), HttpStatus.OK);        
+        }
+    }
 
     @RequestMapping(path = "/api/todo", method = RequestMethod.GET)
     @PreAuthorize("hasRole('USER')")
